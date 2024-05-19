82cb9ec662d36409cf56af4e154f61aeae1835b1
jomin07 <107167492+jomin07@users.noreply.github.com>
2024-05-19 22:52:22 +0530

    view my hotels feature

--- backend/src/routes/my-hotels.ts
+++ backend/src/routes/my-hotels.ts
@@ -16,44 +16,53 @@ const upload = multer({
 });
 
 router.post("/", verifyToken, [
-    body("name").notEmpty().withMessage("Name is required"),
-    body("city").notEmpty().withMessage("City is required"),
-    body("country").notEmpty().withMessage("Country is required"),
-    body("description").notEmpty().withMessage("Description is required"),
-    body("type").notEmpty().withMessage("Hotel type is required"),
-    body("pricePerNight")
-      .notEmpty()
-      .isNumeric()
-      .withMessage("Price per night is required and must be a number"),
-    body("facilities")
-      .notEmpty()
-      .isArray()
-      .withMessage("Facilities are required"),
-], upload.array("imageFiles", 6), async (req: Request, res: Response) =>{
+        body("name").notEmpty().withMessage("Name is required"),
+        body("city").notEmpty().withMessage("City is required"),
+        body("country").notEmpty().withMessage("Country is required"),
+        body("description").notEmpty().withMessage("Description is required"),
+        body("type").notEmpty().withMessage("Hotel type is required"),
+        body("pricePerNight")
+        .notEmpty()
+        .isNumeric()
+        .withMessage("Price per night is required and must be a number"),
+        body("facilities")
+        .notEmpty()
+        .isArray()
+        .withMessage("Facilities are required"),
+    ], upload.array("imageFiles", 6), async (req: Request, res: Response) =>{
+        try {
+            const imageFiles = req.files as Express.Multer.File[];
+            const newHotel: HotelType = req.body;
+
+            const uploadPromises = imageFiles.map(async (image) =>{
+                const b64 = Buffer.from(image.buffer).toString("base64");
+                let dataURI = "data:" + image.mimetype + ";base64," + b64;
+                const res = await cloudinary.v2.uploader.upload(dataURI);
+                return res.url;
+            });
+
+            const imageUrls = await Promise.all(uploadPromises);
+            newHotel.imageUrls = imageUrls;
+            newHotel.lastUpdated = new Date();
+            newHotel.userID = req.userID;
+
+            const hotel = new Hotel(newHotel);
+            await hotel.save();
+
+            res.status(201).send(hotel);
+
+        } catch (error) {
+            console.log("Error creating hotel: ",error);
+            res.status(500).json({ message: "Something went wrong" });
+        }
+});
+
+router.get("/", verifyToken, async (req: Request, res: Response) =>{
     try {
-        const imageFiles = req.files as Express.Multer.File[];
-        const newHotel: HotelType = req.body;
-
-        const uploadPromises = imageFiles.map(async (image) =>{
-            const b64 = Buffer.from(image.buffer).toString("base64");
-            let dataURI = "data:" + image.mimetype + ";base64," + b64;
-            const res = await cloudinary.v2.uploader.upload(dataURI);
-            return res.url;
-        });
-
-        const imageUrls = await Promise.all(uploadPromises);
-        newHotel.imageUrls = imageUrls;
-        newHotel.lastUpdated = new Date();
-        newHotel.userID = req.userID;
-
-        const hotel = new Hotel(newHotel);
-        await hotel.save();
-
-        res.status(201).send(hotel);
-
+        const hotels = await Hotel.find({ userID: req.userID });
+        res.json(hotels);
     } catch (error) {
-        console.log("Error creating hotel: ",error);
-        res.status(500).json({ message: "Something went wrong" });
+        res.status(500).json({ message: "Error fetching hotels" });
     }
 });
 
